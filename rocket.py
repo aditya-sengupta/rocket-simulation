23bdaa901ee76dbd501590d792e36daee518adab
Aditya Sengupta <adiastra@berkeley.edu>
2019-05-29 17:44:40 -0700

    First commit with all the rocket code from /dynamics

---
+++ rocket.py
@@ -0,0 +1,250 @@
+import sys
+sys.path.append("..")
+from dynamic import *
+
+class Parachute:
+
+    CHUTE_OPEN_DELAY = 0.2
+    '''
+    diameter : scalar
+        Diameter of the parachute.
+    cd : scalar
+        Drag coefficient of the parachute.
+    alt : scalar
+        Deployment altitude of the parachute in feet converted to meters.
+        If None, considered to be at apogee.
+    deployed : bool
+        Indicates whether or not the chute has deployed.
+    name : string
+        Name of the parachute, for reading status.
+    t_deployed : scalar
+        Time when the parachute was deployed.
+    '''
+    def __init__(self, diameter, cd, name, alt=None):
+        self.b_drag = 1.275 * np.pi * cd * diameter**2/8
+        if alt is not None:
+            self.alt = alt * 0.3048
+        else:
+            self.alt = float('inf')
+        self.deployed = False
+        self.name = name
+        self.t_deployed = None
+
+    def drag(self, velocity, t):
+        # velocity in m/s
+        # these are random magic numbers
+        if not self.deployed or t - self.t_deployed < self.CHUTE_OPEN_DELAY:
+            return 0
+        return self.b_drag * velocity**2
+
+    def deploy(self, t):
+        if not self.deployed:
+            print("Deploying", self.name, "at t =", str(t).strip(),"s")
+            self.deployed = True
+            self.t_deployed = t
+
+class Motor:
+    # credit for thrust and mass functions: Aled Cuda
+    '''
+    mass_init : scalar
+        Initial mass of the motor.
+    mass_final : scalar
+        Final mass of the motor.
+    time_burnout : scalar
+        Time at which motor burns out in seconds.
+    thrust_curve : string
+        Name of text file containing thrust curve data.
+    time_delay : scalar
+        Time between launch and ignition of this motor in seconds.
+    '''
+    def __init__(self, mass_init, mass_final, thrust_curve, time_delay=0):
+        self.mass_init = mass_init
+        self.mass_final = mass_final
+        self.thrust_data = np.loadtxt(thrust_curve)
+        # adding thrust(t = 0) = 0 to help interpolation
+        self.thrust_data = np.concatenate((np.array([[0,self.thrust_data[::,1][1]/2]]), self.thrust_data), axis=0)
+        self.max_thrust = np.amax(self.thrust_data,0)[1]
+        assert time_delay >= 0, "Cannot have a negative delay."
+        self.time_delay = time_delay
+        # adjust for time delay in thrust data
+        self.thrust_data = self.thrust_data + np.vstack([np.array([time_delay,0])]*self.thrust_data.shape[0])
+        self.time_burnout = np.max(self.thrust_data[::,0]) + time_delay
+
+    def thrust(self, t):
+        # If we ask for a time before or after the range we assume thrust is zero
+        if t > self.time_burnout or t < self.time_delay:
+            return 0
+        # Otherwise we use the interpolate function
+        return interp.interp1d(self.thrust_data[::,0], self.thrust_data[::,1])(t)
+
+    def mass(self, t):
+        if t < self.time_delay:
+            return self.mass_init
+        elif t > self.time_burnout:
+            return self.mass_final
+        return ((self.mass_final - self.mass_init)/(self.time_burnout - self.time_delay))*t + self.mass_init
+
+class Sensor:
+    '''
+    Object that reads in a rocket state variable.
+    select : int
+        Indicates which state variable is being read: 0 for altitude, 1 for velocity (not expected to be used),
+        2 for acceleration.
+    var : scalar
+        Variance of the sensor.
+    data : ndarray
+        Collection of n row vectors with two elements: time and sensor reading.
+    convert : scalar
+        Multiplicative factor from the state variable to the sensor reading. Mostly m to ft conversion.
+    '''
+
+    def __init__(self, select, var, data, convert=3.28):
+        self.select = select
+        self.var = var
+        self.data = data
+        self.convert = convert
+
+class Rocket(DynamicSystem):
+
+    '''
+    A rocket with an altimeter and accelerometer.
+
+    dry_mass : scalar
+        Takes in time, returns rocket mass.
+    parachutes : list
+        A list of Parachute objects.
+    motors : list
+        A list of Motor objects.
+    sensors : list
+        A list of Sensor objects. Setting this to the empty list will set Kalman filtering to simulation without updates.
+    b_drag : scalar
+        Coefficient on v^2 in drag equation. (Not the same as cd.)
+        Currently being determined via curve fitting on OpenRocket sim data.
+    state : ndarray
+        1x3 row vector containing position, velocity, and acceleration in the best units.
+        State represented as a row vector, to be transposed if it's important for matrix operations.
+    apogee : bool
+        Boolean to indicate if apogee has been reached yet, to update parachute states.
+    '''
+    STATE_SIZE = 3
+    INPUT_SIZE = 1
+
+    def __init__(self, dry_mass, parachutes, motors, sensors, b_drag):
+        self.dry_mass = dry_mass
+        self.parachutes = parachutes
+        self.motors = motors
+        self.sensors = sensors
+        self.b_drag = b_drag
+        self.H = np.zeros((len(sensors), 3))
+        self.R = np.zeros((len(sensors), len(sensors)))
+        for i,s in enumerate(sensors):
+            self.H[i][s.select] = s.convert
+            self.R[i][i] = s.var
+        self.P = np.zeros([self.STATE_SIZE, self.STATE_SIZE])
+        q = 2
+        d = 0.001 # sampling time difference of one of the sensors. Just in here for the noise model, to be updated.
+        # constant acceleration approximation: change this later
+        self.Q = q * np.array([[d**4/4, d**3/3, d**2/2], [d**3/3, d**2/2, d], [d**2/2, d, 1]])
+        # for now these are hardcoded in, but find a way to remove them and still detect apogee:
+        self.stdev_alt = np.sqrt(5.54)
+        self.stdev_acc = np.sqrt(62.953)
+        self.apogee = False
+        self.simend = False
+        self.max_thrust = sum([m.max_thrust for m in self.motors])
+        self.state = np.array([0, 0, 0])
+        self.state[2] = self.get_thrust(0)/self.get_mass(0)
+        print(self.state)
+
+    def get_mass(self, t):
+        return self.dry_mass + sum([m.mass(t) for m in self.motors])
+
+    def get_thrust(self, t):
+        # to do: burnout detection.
+        return sum([m.thrust(t) for m in self.motors])
+
+    def get_rocket_drag(self):
+        magnitude = self.b_drag * self.state[1]**2
+        if not self.apogee:
+            return -magnitude
+        else:
+            return magnitude
+
+    def get_chute_drag(self, t):
+        return sum([p.drag(self.state[1], t) for p in self.parachutes])
+
+    def ext_input(self, t, mass=None):
+        if mass is None:
+            mass = self.get_mass(t)
+        thrust = self.get_thrust(t)
+        if thrust == 0 and t < 0.2:
+            # to do: better way of detecting 'still on the pad'
+            gravity = 0
+        else:
+            gravity = -9.8 * mass
+        rocket_drag = self.get_rocket_drag()
+        if thrust > 0.05*self.max_thrust:
+            assert thrust > np.abs(rocket_drag), "Excessive drag"
+        chute_drag = self.get_chute_drag(t)
+        return (thrust + gravity + rocket_drag + chute_drag, str([thrust, gravity, rocket_drag, chute_drag]) + '\n')
+
+    def evolve(self, t, dt, order=2):
+        # order: of integration.
+        m = self.get_mass(t)
+        if order == 1:
+            A = np.array([[1, dt, dt**2/2],[0, 1, dt],[0, 0, 0]])
+            B = np.array([0, 0, 1/m])
+        elif order == 2:
+            A = np.array([[1, dt, dt**2/2], [0, 1, dt/2], [0, 0, 0]])
+            B = np.array([0, dt/(2*m), 1/m])
+        # Orders > 2 to be implemented based on https://math.stackexchange.com/questions/2946737/solving-a-matrix-differential-equation-using-runge-kutta-methods, plus some slightly jank workarounds that allow the 'a' row to still be all zero.
+        # Check for apogee
+        if not self.apogee:
+            try:
+                assert self.state[0] > -self.stdev_alt, "you will not go to space today"
+            except AssertionError as e:
+                e.args += ("Time", t, "State", self.state)
+                raise
+            if self.state[1] < -self.stdev_acc and self.state[2] < -self.stdev_acc:
+                # this is kind of a jank apogee check, especially because I'm comparing velocity to a deviation in acceleration
+                try:
+                    assert self.get_thrust(t) == 0, "If you're at apogee, the motor can't still be burning"
+                except AssertionError as e:
+                    e.args += ("Time", t, "thrust", self.get_thrust(t), "state:", self.state)
+                    raise
+                self.apogee = True
+                print("Hit apogee at altitude",str(np.round(3.28*self.state[0], 2)),"ft")
+                for p in self.parachutes:
+                    if p.alt is None:
+                        p.deploy(t)
+        # Check for altitude-based chute deployment
+        # To do: nicer way of doing this than checking at every timestep.
+        if self.apogee and not all([p.deployed for p in self.parachutes]):
+            for p in self.parachutes:
+                if p.alt > self.state[0]:
+                    p.deploy(t)
+
+        # Check for ground hit
+        if self.apogee and self.state[0] < self.stdev_alt and A.dot(self.state)[0] < -2*self.stdev_alt:
+            self.simend = True
+        return (A, B)
+
+    def reset(self):
+        # for some reason copy.deepcopy isn't working so here
+        for p in self.parachutes:
+            p.deployed = False
+            p.t_deployed = None
+        self.apogee = False
+        self.state = np.array([0,0,0])
+        self.simend = False
+
+    def status(self):
+        print("Altitude:", self.state[0])
+        print("Velocity:", self.state[1])
+        print("Acceleration:", self.state[2])
+        print("Apogee hit:", self.apogee)
+        for p in self.parachutes:
+            print(p.name, "deployed:", p.deployed)
+        print("Ground hit:", self.simend)
+
+    def compact_status(self, t):
+        print(t, self.state, self.apogee, [p.deployed for p in self.parachutes], self.simend)
---
+++ subarktos/subarktos.py
@@ -0,0 +1,147 @@
+import sys
+sys.path.append("..")
+from rocket import *
+from scipy.integrate import quad
+
+def get_subarktos():
+    mass = 4.42 #kg
+    motor = Motor(1.223, 0.713, "J450.txt")
+    drogue = Parachute(0.3, 1.8, "Drogue")
+    main = Parachute(0.9, 2.2, "Main", alt=800)
+    altimeter = Sensor(0, 5.54, None)
+    accelerometer = Sensor(2, 62.953, None)
+    drag = 0.000292
+    subarktos = Rocket(mass, [drogue, main], [motor], [altimeter, accelerometer], drag)
+    return subarktos
+
+def get_state_data():
+    sim = pd.read_csv("subarktos_sim.csv", comment='#')
+    read_t = np.array(sim['Time (s)'])
+    read_alt = np.array(sim['Altitude (ft)'])
+    read_vel = np.array(sim['Vertical velocity (ft/s)'])
+    read_acc = np.array(sim['Vertical acceleration (ft/s^2)'])
+    return (read_t, read_alt, read_vel, read_acc)
+
+def get_force_data():
+    sim = pd.read_csv("subarktos_sim.csv", comment='#')
+    read_t = np.array(sim['Time (s)'])
+    t_apogee = 16.16 # read off csv
+    read_drag = np.array(sim['Drag force (N)'])
+    read_thrust = np.array(sim['Thrust (N)'])
+    read_gravity = np.array(sim['Gravitational acceleration (ft/s^2)']) * np.array(sim['Mass (oz)']) * 0.008641
+    def force(t):
+        n = np.where(read_t == t)[0][0]
+        if t < t_apogee:
+            return read_thrust[n] - read_drag[n] - read_gravity[n]
+        else:
+            return read_drag[n] - read_gravity[n]
+    return (np.vectorize(force))(read_t)
+
+def test_acc_differences(subarktos, read_t, read_acc):
+    # question: if you integrate the same acceleration data, do you get the same velocity and altitude?
+    # answer: you do!
+    def new_ext_input(t):
+        if t < np.min(read_t) or t > np.max(read_t):
+            return 0
+        return (interp.interp1d(read_t, read_acc)(t)/3.28 * subarktos.get_mass(t), "OpenRocket sim data")
+
+    subarktos.ext_input = new_ext_input
+    return subarktos
+
+def sim_and_plot(subarktos, read_t, read_alt, read_vel, read_acc, read_force):
+    plt.figure()
+    times, states, inputs = subarktos.simulate(dt=0.001, timeout=120, verbose=False)
+
+    print(states[1][2300:2400])
+
+    plt.subplot(2,2,1)
+    plt.plot(times, 3.28*states[0], label='subarktos.simulate()')
+    plt.plot(read_t, read_alt, label='OpenRocket simulation')
+    plt.title("Altitude over time from subarktos.simulate() and OpenRocket")
+    plt.legend(loc='upper right')
+    plt.xlabel("Time (s)")
+    plt.ylabel("Altitude (ft)")
+
+    plt.subplot(2,2,2)
+    plt.plot(times, 3.28*states[1], label='subarktos.simulate()')
+    plt.plot(read_t, read_vel, label='OpenRocket simulation')
+    plt.title("Velocity over time from subarktos.simulate() and OpenRocket")
+    plt.legend(loc='upper right')
+    plt.xlabel("Time (s)")
+    plt.ylabel("Velocity (ft/s)")
+
+    plt.subplot(2,2,3)
+    plt.plot(times, 3.28*states[2], label='subarktos.simulate()')
+    plt.plot(read_t, read_acc, label='OpenRocket simulation')
+    plt.title("Acceleration over time from subarktos.simulate() and OpenRocket")
+    plt.legend(loc='upper right')
+    plt.xlabel("Time (s)")
+    plt.ylabel("Acceleration (ft/s^2)")
+
+    plt.subplot(2,2,4)
+    plt.plot(times, inputs[0], label='subarktos.simulate()')
+    plt.plot(read_t, read_force, label='OpenRocket simulation')
+    plt.title("Total force over time from subarktos.simulate() and OpenRocket")
+    plt.legend(loc='upper right')
+    plt.xlabel("Time (s)")
+    plt.ylabel("Force (N)")
+    plt.show()
+
+def test_kalman_openrocket(subarktos, read_t, read_alt, read_acc):
+    sigma_alt = np.max(read_alt)/64
+    sigma_acc = np.max(read_acc)/64
+    alt_noise = np.random.normal(0, sigma_alt, np.size(read_alt))
+    acc_noise = np.random.normal(0, sigma_acc, np.size(read_acc))
+    sensor_data = np.vstack([(read_alt + alt_noise, read_acc + acc_noise)])
+    filtered_times, filtered_states, _ = subarktos.simulate(dt=0.001, timeout=120, kalman=(read_t, sensor_data.T))
+
+    def interpolate_state_data(data):
+        def toreturn(t):
+            if t < np.min(read_t) or t > np.max(read_t):
+                return 0
+            return interp.interp1d(read_t, data)(t)
+        return toreturn
+
+    alt_error_after = 3.28*filtered_states[0] - np.vectorize(interpolate_state_data(read_alt))(filtered_times)
+    acc_error_after = 3.28*filtered_states[2] - np.vectorize(interpolate_state_data(read_acc))(filtered_times)
+
+    plt.figure()
+
+    plt.subplot(2,2,1)
+    plt.plot(read_t, read_alt, label="OpenRocket without noise")
+    plt.plot(filtered_times, 3.28*filtered_states[0], label="subarktos.simulate() filtered from OpenRocket + noise")
+    plt.legend(loc='upper right')
+    plt.title("Kalman filtered rocket altitude against true values.")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Altitude (ft)")
+
+    plt.subplot(2,2,2)
+    plt.plot(read_t, read_acc, label="OpenRocket without noise")
+    plt.plot(filtered_times, 3.28*filtered_states[2], label="subarktos.simulate() filtered from OpenRocket + noise")
+    plt.legend(loc='upper right')
+    plt.title("Kalman filtered rocket acceleration against true values.")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Acceleration (ft/s^2)")
+
+    plt.subplot(2,2,3)
+    plt.plot(read_t, alt_noise, label="Error before filtering")
+    plt.plot(filtered_times, alt_error_after, label="Error after filtering")
+    plt.title("Altitude error reduction due to Kalman filtering.")
+    plt.legend(loc='upper right')
+    plt.xlabel("Time (s)")
+    plt.ylabel("Error in altitude (ft)")
+
+    plt.subplot(2,2,4)
+    plt.plot(read_t, acc_noise, label="Error before filtering")
+    plt.plot(filtered_times, acc_error_after, label="Error after filtering")
+    plt.title("Acceleration error reduction due to Kalman filtering.")
+    plt.legend(loc='upper right')
+    plt.xlabel("Time (s)")
+    plt.ylabel("Error in acceleration (ft/s^2)")
+
+    plt.show()
+
+subarktos = get_subarktos()
+state_data = get_state_data()
+force_data = get_force_data()
+sim_and_plot(subarktos, *state_data, force_data)
